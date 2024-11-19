f5a6a9489d686accc2ca101d1168ea04f5b63956
Chirag Jain <dungarwalsaab@gmail.com>
2024-11-19 22:17:44 +0530

    Added ui changes to make font responsive for TopPage

--- src/Components/TopPage.js
+++ src/Components/TopPage.js
@@ -1,34 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import Typewriter from 'typewriter-effect';
- import NavBar from './NavBar_Section.js/NavBar';
 import BackGroundCircle from './BackGroundCircle';
 import {motion} from "framer-motion";
-import {useState} from "react";
-// import styles from './social.module.scss'
 
 function TopPage() {
-    const[curr,setCurr] = useState(1);
-    const handleHover = (e,id)=>{
-        console.log("enter")
-        let arr =document.getElementById(id+"a");
-        let irr = document.getElementById(id+"I");
-        let crr = document.getElementById(id+"II");
-        irr.style.display="block";
-        arr.style.display="flex";
-        crr.style.display="none";
-    }
-
-    const handleEffect = (e,id)=>{
-        console.log("remove")
-        let arr =document.getElementById(id+"a");
-        let irr = document.getElementById(id+"I");
-        let crr = document.getElementById(id+"II");
-        irr.style.display="none";
-        arr.style.display="none";
-        crr.style.display="block";
-    }
-
      
   return (
     <Container>
@@ -41,13 +17,13 @@ function TopPage() {
                 </motion.ul>
             </Social>
             <BackGround>
-                <motion.h1
+                <motion.div
                     initial={{opacity:0,scale:1.4}} animate={{opacity:1,scale:1}}
                     transition= {{ duration:3}} 
                 >
                 <p>Hello</p>
                 <p>World !!</p>
-                </motion.h1>
+                </motion.div>
             </BackGround>
             <Content className="animate__animated animate__lightSpeedInLeft animate__delay-2s">
                     <div >Hi I'm</div>
@@ -139,10 +115,8 @@ const Social = styled.div`
 const Container = styled.div`
     position:relative;
     scroll-snap-align: start;
-    // background-color: #0b0a0a;
     height:100vh;
-    width:100vw;
-    // 
+    width:100vw; 
 `;
 const BackGround =  styled.div`
     position: absolute;
@@ -155,336 +129,58 @@ const BackGround =  styled.div`
     width: 90vw;
     // font-weight:700;
     z-index:-1;
-    h1{
-        font-size: 30rem;
+    div{
+        font-size: 70rem;
         font-weight: 700;
         color: transparent;
-        /* text-shadow:0.01em 0.01em 0.02em rgb(0 0 0 / 25%), 0 0 0 #111111;  #191919*/
         text-shadow: 0.02em 0.02em 0.01em rgb(0 0 0 / 25%), 0 0 0 #1f1d1d;
         -webkit-text-stroke: 0.5px #343434;
+        p{
+            line-height: 1;
+            padding: 0;
+            margin: 0;
+        }
     }
-    @media only screen and (max-width: 1025px) {
-        h1 {
+    @media only screen and (max-width: 3000px) {
+        div {
+            font-size: 55rem;
+        }
+    }
+    @media only screen and (max-width: 2450px) {
+        div {
+            font-size: 48rem;
+        }
+    }
+    @media only screen and (max-width: 2150px) {
+        div {
+            font-size: 40rem;
+        }
+    }
+    @media only screen and (max-width: 1700px) {
+        div {
+            font-size: 34rem;
+        }
+    }
+    @media only screen and (max-width: 1450px) {
+        div {
+            font-size: 28rem;
+        }
+    }
+    @media only screen and (max-width: 1300px) {
+        div {
             font-size: 24rem;
         }
-      }
-`;
-
-
-const Cards = styled.div`
-    position: absolute;
-    height: 31rem;
-    width: 45rem;
-    top: 6rem;
-    right: 10rem;
-    z-index:2;
-    // border: 0.5px solid #343434;
-    @media only screen and (max-width: 1030px) {
-        height: 33rem;
-        width: 33rem;
-        top: 10rem;
-        right: 0rem;
-        display:flex;
-        flex-direction:column;
-        justify-content: space-around;
     }
-    @media only screen and (max-width: 770px) {
-         display:none;
-    }
-`;
-const Card1 = styled.div`
-    position: absolute;
-    height: 80%;
-    width: 40%;
-    left: 25%;
-    transform: rotate(8deg);
-    border: 0.5px solid #343434;
-    background: transparent;
-    
-    // opacity:0.5;
-    transition: 0.3s;
-    box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-    Img:nth-of-type(1){
-        height: 25%;
-        width: 35%;
-        // filter: grayscale(1);
-        filter: contrast(0.1);
-        position: absolute;
-        top: 30%;
-        left: 30%;
-        
-        animation-name: rotateD;
-        animation-duration: 5s;
-        animation-iteration-count: infinite;
-        animation-timing-function: ease;
-        animation-direction: reverse;
-        @keyframes rotateD {
-            0%{
-                scale:0.8;
-                transform: rotate3d(0, 1, 0,0deg);
-            }
-            25%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,90deg);
-            }
-            50%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,180deg);
-            }
-            75%{
-                scale:0.8;
-                transform: rotate3d(0, 1, 0,270deg);
-            }
-            100%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,360deg);
-            }
-        }
-    }
-    Img:nth-of-type(2){
-        display:none;
-        position:absolute;
-        height: 100%;
-        width: 100%;
-        // filter:brightness(0.5);
-        &:hover{
-            opacity:0.4;
-             
-        }
-    }
-    div{
-        display:none;
-        height: 100%;
-        width: 100%;
-        text-align: center;
-        // display: flex;
-        align-items: center;
-         
-        font-style: oblique;
-        font-weight: 700;
-        color:black;
-        color: black;
-        font-size: 2rem;
-        -webkit-text-stroke: 0.7px white;
-        // text-shadow: 0px 2px 20px rgba(203, 213, 43, 0.53);
-        text-shadow: 0px 0px 5px #fea314;
-    }
-     
-    &:hover {
-        z-index:2;
-        width:50%;
-        height:80%;
-        cursor:pointer;
-        transform: rotate(0deg);
-    }
-
-    @media only screen and (max-width: 1025px) {
-        //display:none;
-        height:70%;
-        width:40%;
-        left:30%;
-    }
-    @media only screen and (max-width: 770px) {
-        display:none;
-   }
-`;
-const Card2 = styled.div`
-    position: absolute;
-    height: 80%;
-    width: 40%;
-    left: -5%;
-    top: 14%;
-    // opacity:0.5;
-    transform: rotate(-6deg);
-    border: 0.5px solid #343434;
-    background: transparent;
-    transition: 0.3s;
-    box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-    &:hover {
-        z-index:2;
-        width:50%;
-        height:80%;
-        cursor:pointer;
-        transform: rotate(0deg);
-        transform: translateX(10px);
-    }
-    Img:nth-of-type(1){
-        height: 22%;
-        width: 30%;    
-        position: absolute;
-        top: 30%;
-        filter: contrast(0.5);
-        left: 30%;
-        &:hover{
-            display:none;
-        }
-
-        animation-name: rotateD;
-        animation-duration: 5s;
-        animation-iteration-count: infinite;
-        animation-timing-function: ease;
-        animation-direction: reverse;
-        @keyframes rotateD {
-            0%{
-                scale:0.8;
-                transform: rotate3d(0, 1, 0,0deg);
-            }
-            25%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,90deg);
-            }
-            50%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,180deg);
-            }
-            75%{
-                scale:0.8;
-                transform: rotate3d(0, 1, 0,270deg);
-            }
-            100%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,360deg);
-            }
-        }
-    }
-    Img:nth-of-type(2){
-        display:none;
-        position:absolute;
-        height: 100%;
-        background-position: center; 
-        width: 100%;
-        // filter:brightness(0.5);
-        &:hover{
-            opacity:0.5;
-            filter: grayscale(0.4);
-        }
-    }
-    div{
-        display:none;
-        height: 100%;
-        width: 100%;
-        text-align: center;
-        // display: flex;
-        align-items: center;
-        font-size: 2rem;
-        font-style: oblique;
-        font-weight: 700;
-        -webkit-text-stroke: 0.7px #ffffff;
-        // text-shadow: 0px 2px 20px rgba(203, 213, 43, 0.53);
-        text-shadow: 0px 0px 5px #12e4d9;
-    }
-    @media only screen and (max-width: 1025px) {
-       // display:none;
-       height:70%;
-       width: 40%;
-        left: -7%;
-        top: 22%;
-        transform: rotate(15deg);
-    }
-    @media only screen and (max-width: 770px) {
-        display:none;
-   }
- `;
-const Card3 = styled.div`
-    position: absolute;
-    height: 80%;
-    width: 40%;
-    right: 10%;
-    top: 31%;
-    // opacity:0.5;
-    transform: rotate(14deg);
-    border: 0.5px solid #343434;
-    background: transparent;
-    transition: 0.3s;
-    box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-    &:hover { 
-        z-index:2;
-        width:50%;
-        height:80%;
-        cursor:pointer;
-        transform: rotate(0deg);
-        transform: translateX(-10px);
-    }
-    div{
-        display:none;
-        height: 100%;
-        width: 100%;
-        text-align: center;
-        // display: flex;
-        align-items: center;
-        font-size: 2rem;
-        font-style: oblique;
-        font-weight: 700;
-        -webkit-text-stroke: 0.7px white;
-        // text-shadow: 0px 2px 20px rgba(203, 213, 43, 0.53);
-        text-shadow: 0px 0px 5px #fea314;
-        @media only screen and (max-width: 1025px) {
-            //display:none;
-        }
-    }
-    Img:nth-of-type(1){
-        height: 30%;
-        width: 40%;
-        filter: grayscale(1);
-        position: absolute;
-        top: 30%;
-        left: 30%;
-        filter: contrast(0.5);
-        &:hover{
-            display:none;
-        }
-        animation-name: rotateD;
-        animation-duration: 5s;
-        animation-iteration-count: infinite;
-        animation-timing-function: ease;
-        animation-direction: reverse;
-        @keyframes rotateD {
-            0%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,0deg);
-            }
-            25%{
-                scale:0.8;
-                transform: rotate3d(0, 1, 0,90deg);
-            }
-            50%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,180deg);
-            }
-            75%{
-                scale:0.8;
-                transform: rotate3d(0, 1, 0,270deg);
-            }
-            100%{
-                scale:1;
-                transform: rotate3d(0, 1, 0,360deg);
-            }
-        }
-    }
-    Img:nth-of-type(2){
-        display:none;
-        position:absolute;
-        height: 100%;
-        width: 100%;
-        // filter:brightness(0.5);
-        &:hover{
-            opacity:0.5;
-            filter: grayscale(0.4);
-        }
-        @media only screen and (max-width: 1025px) {
-            display:none;
+    @media only screen and (max-width: 1150px) {
+        div {
+            font-size: 24rem;
         }
     }
     @media only screen and (max-width: 1025px) {
-        //display:none
-        height:70%;
-        width:40%;
-        right:6%;
-        transform: rotate(-10deg);
+        div {
+            font-size: 22rem;
+        }
     }
-    @media only screen and (max-width: 770px) {
-        display:none;
-   }
 `;
  
 
@@ -492,19 +188,20 @@ const Content = styled.div`
     position: absolute;
     top: 24%;
     left: 8%;
-    height: 18rem;
-    width: 60rem;
+    height: fit-content;
+    width: 100%;
     color: white;
+    font-size: 25rem;
+    font-family: emoji;
+    font-weight: 700;
+    color: rgb(243,243,243);
     div{
-        font-size: 9rem;
-        // font-family: 'Titillium Web', sans-serif;
-        font-family: emoji;
-        font-weight: 700;
-        color: rgb(243,243,243);
         display: flex;
         align-items: center;
-        
-        color: transparent;
+        color: transparent;    
+        line-height: 1.2;
+        padding: 0;
+        margin: 0;
         /* stroke: wheat; */
         -webkit-text-stroke: 0.7px #b8b8b8;  //#e9b171;  #2d9687
     }
@@ -518,6 +215,43 @@ const Content = styled.div`
             color:#e0c1a9;
         } 
     }
+
+    @media only screen and (max-width: 3000px) {
+        div {
+            font-size: 22rem;
+        }
+    }
+    @media only screen and (max-width: 2450px) {
+        div {
+            font-size: 17rem;
+        }
+    }
+    @media only screen and (max-width: 2150px) {
+        div {
+            font-size: 17rem;
+        }
+    }
+    @media only screen and (max-width: 1700px) {
+        div {
+            font-size: 15rem;
+        }
+    }
+    @media only screen and (max-width: 1450px) {
+        div {
+            font-size: 12rem;
+        }
+    }
+    @media only screen and (max-width: 1300px) {
+        div {
+            font-size: 10rem;
+        }
+    }
+    @media only screen and (max-width: 1150px) {
+        div {
+            font-size: 9rem;
+        }
+    }
+    
     @media only screen and (max-width: 769px) {
         div{
             font-size: 6rem;
